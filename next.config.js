c8f8de1508d9a10be2210b45b66712ed31610ef8
Krishna Prajapati <kkp785216@gmail.com>
2023-06-04 12:51:52 +0530

    env updated into next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    // Reference a variable that was defined in the .env.* file and make it available at Build Time
+    NEXT_PUBLIC_UNPROTECTED_API_BASE_PATH:
+      process.env.NEXT_PUBLIC_UNPROTECTED_API_BASE_PATH,
+    NEXT_PUBLIC_AUTH_API_BASE_PATH: process.env.NEXT_PUBLIC_AUTH_API_BASE_PATH,
+    NEXT_PUBLIC_ADMIN_API_BASE_PATH:
+      process.env.NEXT_PUBLIC_ADMIN_API_BASE_PATH,
+    NEXT_PUBLIC_USER_API_BASE_PATH: process.env.NEXT_PUBLIC_USER_API_BASE_PATH,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
